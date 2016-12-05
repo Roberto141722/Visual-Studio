4ce588b0dc656ba1f94c1fa6731d83351921c546
Roberto141722 <park_uchiha141722@hotmail.com>
2016-12-04 22:32:41 -0600

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Dim a, b, r As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        r = (a + b)
+        TextBox3.Text = r
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        r = (a - b)
+        TextBox3.Text = r
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        r = (a * b)
+        TextBox3.Text = r
+    End Sub
+End Class
